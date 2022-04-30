175a94f0d56f1593c3eaea8ae2ab14523f16db62
Marlon Salles <marlonsalles@hotmail.com>
2022-04-30 20:50:15 -0300

    File, FileInfo & IOException

---
+++ File_e_FileInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO; // Realiza Operações com arquivos -> FileStream : É um objeto a sequencia de leitura ou escrita de um recurso de IO / Arquivo
+                 //File : Operações Estaticas 
+                 //FileInfo : Instanciação de objeto necessaria. Porem mais perfomartico. 
+namespace File_e_FileInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\marlo\AppData\Local\Temp\arquivo.txt"; //Caminho do arquivo ja criado  
+            string targetPath = @"C:\Users\marlo\AppData\Local\Temp\arquivo2.txt"; 
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath); //Instanciãção de objeto associado ao sourcePath
+                fileInfo.CopyTo(targetPath);
+                string[] lines =  File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                string a = Console.ReadLine();
+            }
+        }
+    }
+}
